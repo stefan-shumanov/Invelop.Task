52d768fe3c4d6d784cedb474ea81a57a060a27c3
Ste <Ste@DESKTOP-5OUHBRB>
2021-12-13 00:59:23 +0200

    Add project files.

---
+++ Invelop.Task/DAL/PersonContext.cs
@@ -0,0 +1,22 @@
+﻿using Invelop.Task.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Invelop.Task.DAL
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext() : base("PersonContext")
+        {
+            Database.Connection.Open();
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<PersonContext>());
+        }
+
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Invelop.Task/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Invelop.Task.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        public string Surname { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [MaxLength(15)]
+        [MinLength(6)]
+        [RegularExpression("^([0-9]{6,15} *)$", ErrorMessage = "Phone Number must be numeric and between 6 and 15 symbols long.")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "IBAN")]
+        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "IBAN must contain only netters and numbers.")]
+        public string Iban { get; set; }
+    }
+}
